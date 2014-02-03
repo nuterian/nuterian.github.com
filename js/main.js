984d079af30ade7e1be52175f466eda62e2442d9
Jugal Manjeshwar <jugalm9@gmail.com>
2014-02-03 14:45:27 -0500

    Update Navigation UI Add 'About' page Add smooth scrolling using jQuery Update Portfolio previews to jpgs, increased dimensions and blur on hover

--- js/main.js
+++ js/main.js
@@ -38,3 +38,64 @@ for(var i=0; i<previews.length; i++){
 		e.preventDefault();
 	});
 }
+
+$(document).ready(function(){
+
+    /** 
+     * This part does the "fixed navigation after scroll" functionality
+     * We use the jQuery function scroll() to recalculate our variables as the 
+     * page is scrolled/
+     */
+    $(window).scroll(function(){
+        var window_top = $(window).scrollTop(); // the "12" should equal the margin-top value for nav.stick
+    });
+
+    /**
+     * This part causes smooth scrolling using scrollto.js
+     * We target all a tags inside the nav, and apply the scrollto.js to it.
+     */
+    $("#nav a").click(function(e){
+        e.preventDefault();
+        $('html,body').scrollTo(this.hash, this.hash); 
+    });
+
+    /**
+     * This part handles the highlighting functionality.
+     * We use the scroll functionality again, some array creation and 
+     * manipulation, class adding and class removing, and conditional testing
+     */
+    var aChildren = $("#nav a"); // find the a children of the list items
+    var aArray = []; // create the empty aArray
+    for (var i=0; i < aChildren.length; i++) {    
+        var aChild = aChildren[i];
+        var ahref = $(aChild).attr('href');
+        aArray.push(ahref);
+    } // this for loop fills the aArray with attribute href values
+
+    var $curPage = aArray[0];
+    
+    $(window).scroll(function(){
+        var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
+        var windowHeight = $(window).height(); // get the height of the window
+        var docHeight = $(document).height();
+
+        for (var i=0; i < aArray.length; i++) {
+            var theID = aArray[i];
+            var divPos = $(theID).offset().top; // get the offset of the div from the top of page
+            var divHeight = $(theID).height(); // get the height of the div in question
+            if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
+                $("a[href='" + theID + "']").addClass("current");
+            } else {
+                $("a[href='" + theID + "']").removeClass("current");
+            }
+        }
+
+        if(windowPos + windowHeight == docHeight) {
+            if (!$("#nav a:last-child").hasClass("current")) {
+                var navActiveCurrent = $(".current").attr("href");
+                $("a[href='" + navActiveCurrent + "']").removeClass("current");
+                $("#nav a:last-child").addClass("current");
+            }
+        }
+    });
+});
